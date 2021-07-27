2dff965111279a25020bd8997e3c58b21e905f7d
EssienNsikak <nsikak.essienceo@gmail.com>
2021-07-27 15:55:35 +0100

    backend server setup

--- api/index.js
+++ api/index.js
@@ -2,3 +2,44 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app = express();
+
+dotenv.config();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  return next();
+});
+
+const MONGO_URL = process.env.MONGO_URL
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB Connected Successfully'))
+  .catch((err) => {
+    console.error(err.message);
+  });
+
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Backend server is running on PORT ${PORT}`);
+});
