6bc107ef1d4e834ac133d090ad8c0842503b3a1b
EssienNsikak <nsikak.essienceo@gmail.com>
2021-08-02 16:59:18 +0100

    pagination implemented on the backend and other minor fixes

--- api/controllers/postController.js
+++ api/controllers/postController.js
@@ -6,14 +6,34 @@ import Post from '../models/Post.js';
 const router = express.Router();
 
 export const getPosts = async (req, res) => {
-  try {
-    const posts = await Post.find();
+  const { page } = req.query;
 
-    res.status(200).json(posts);
+  try {
+    const LIMIT = 8;
+    const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of every page
+
+    const total = await Post.countDocuments({});
+    const posts = await Post.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex);
+
+    res.json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT) });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
-};
+}
+
+export const getPostsBySearch = async (req, res) => {
+  const { searchQuery, tags } = req.query;
+
+  try {
+    const title = new RegExp(searchQuery, 'i');
+
+    const posts = await Post.find({ $or: [{ title }, { tags: { $in: tags.split(',') } }] });
+
+    res.json({ data: posts });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+}
 
 export const getPost = async (req, res) => {
   const { id } = req.params;
@@ -25,7 +45,7 @@ export const getPost = async (req, res) => {
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
-};
+}
 
 export const createPost = async (req, res) => {
   const post = req.body;
@@ -39,7 +59,7 @@ export const createPost = async (req, res) => {
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
-};
+}
 
 export const updatePost = async (req, res) => {
   const { id } = req.params;
@@ -52,7 +72,7 @@ export const updatePost = async (req, res) => {
   await Post.findByIdAndUpdate(id, updatedPost, { new: true });
 
   res.json(updatedPost);
-};
+}
 
 export const deletePost = async (req, res) => {
   const { id } = req.params;
@@ -62,7 +82,7 @@ export const deletePost = async (req, res) => {
   await Post.findByIdAndRemove(id);
 
   res.json({ message: 'Post deleted successfully.' });
-};
+}
 
 export const likePost = async (req, res) => {
   const { id } = req.params;
