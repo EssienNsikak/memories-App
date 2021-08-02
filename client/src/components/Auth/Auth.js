a9a05fb79f029aec313c3e76b9231eeb4e714fb4
EssienNsikak <nsikak.essienceo@gmail.com>
2021-08-02 17:20:43 +0100

    auth, navabar, home and form components fixed

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -52,13 +52,13 @@ const SignUp = () => {
     }
   };
 
-  const googleError = () => alert('Google Sign In was unsuccessful. Try again later');
+  const googleError = () => console.log('Google Sign In was unsuccessful. Try again later');
 
   const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
 
   return (
     <Container component='main' maxWidth='xs'>
-      <Paper className={classes.paper} elevation={3}>
+      <Paper className={classes.paper} elevation={6}>
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
         </Avatar>
@@ -67,55 +67,21 @@ const SignUp = () => {
           <Grid container spacing={2}>
             { isSignup && (
             <>
-              <Input 
-                name='firstName' 
-                label='First Name' 
-                handleChange={handleChange} 
-                autoFocus half 
-              />
-              <Input 
-                name='lastName' 
-                label='Last Name' 
-                handleChange={handleChange} 
-                half 
-              />
+              <Input name='firstName' label='First Name' handleChange={handleChange} autoFocus half />
+              <Input name='lastName' label='Last Name' handleChange={handleChange} half />
             </>
             )}
-            <Input 
-              name='email' 
-              label='Email Address' 
-              handleChange={handleChange} 
-              type='email' 
-            />
-            <Input 
-              name='password' 
-              label='Password' 
-              handleChange={handleChange} 
-              type={showPassword ? 'text' : 'password'} 
-              handleShowPassword={handleShowPassword} 
-            />
+            <Input name='email' label='Email Address' handleChange={handleChange} type='email' />
+            <Input name='password' label='Password' handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
             { isSignup && <Input name='confirmPassword' label='Repeat Password' handleChange={handleChange} type='password' /> }
           </Grid>
-          <Button 
-            type='submit' 
-            fullWidth 
-            variant='contained' 
-            color='primary' 
-            className={classes.submit}
-          >
+          <Button type='submit' fullWidth variant='contained' color='primary' className={classes.submit}>
             { isSignup ? 'Sign Up' : 'Sign In' }
           </Button>
           <GoogleLogin
             clientId='668869318980-qa832kfsat57np1u1b8gidg4g8qq8312.apps.googleusercontent.com'
             render={(renderProps) => (
-              <Button 
-                className={classes.googleButton} 
-                color='primary' 
-                fullWidth onClick={renderProps.onClick} 
-                disabled={renderProps.disabled} 
-                startIcon={<Icon />} 
-                variant='contained'
-              >
+              <Button className={classes.googleButton} color='primary' fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon />} variant='contained'>
                 Google Sign In
               </Button>
             )}
@@ -126,7 +92,7 @@ const SignUp = () => {
           <Grid container justify='flex-end'>
             <Grid item>
               <Button onClick={switchMode}>
-                { isSignup ? 'Already have an account? Sign in' : "Don't have an account? Sign Up" }
+                { isSignup ? "Already have an account? Sign in' : 'Don't have an account? Sign Up" }
               </Button>
             </Grid>
           </Grid>
